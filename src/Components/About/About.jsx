26a49baece101e09e121108213e480fbb7e05e4d
FelipeJure <felipe.jure05@gmail.com>
2023-07-25 19:12:04 +0200

    update about

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -14,10 +14,13 @@ export default function About() {
         {language === "english" ? (
           <>
             <p>
-              This year I took a turn in my career and entered the IT world. It
+              The last year I took a turn in my career and entered the IT world. It
               was a difficult but very satisfying decision and today I am
-              grateful and surprised with the progress and skills I acquired. I
-              am responsible and dedicated. I like to travel and learn about
+              grateful and surprised with the progress and skills I acquired. 
+              <br/>
+              While I have a stronger affinity for Front-End, I feel very comfortable working on the Back-End, so my goal is to continue developing in both areas.
+              <br/>
+              I am responsible and dedicated. I like to travel and learn about
               other cultures.
             </p>
             <p>Why web development?</p>
@@ -31,16 +34,17 @@ export default function About() {
         ) : (
           <>
             <p>
-              Este año di un giro en mi carrera y entré en el mundo de la IT.
-              Fue una decisión difícil pero muy satisfactoria y hoy estoy
-              agradecido y sorprendido con los progresos y las habilidades que
-              adquirí. Soy responsable y dedicado. Me gusta viajar y conocer
-              otras culturas.
+            El año pasado di un giro a mi carrera y me introduje en el mundo IT. Fue una decisión difícil pero muy satisfactoria y hoy estoy agradecido y sorprendido con los avances y las capacidades que adquirí.
+            <br/>
+            Si bien tengo mayor afinidad por el Front-End, me siento muy cómodo trabajando en el Back-End por lo que mi meta es seguir desarrollándome en las dos áreas.
+            <br/>
+            Soy responsable y dedicado. Me gusta viajar y conocer otras culturas. Soy sociable y extrovertido.
             </p>
             <p>¿Por qué desarrollo web?</p>
             <p>
-              Tengo mucha curiosidad y energías para aprender cosas nuevas. El
-              desarrollo web es un área que me fascina por la capacidad de poder
+              Tengo mucha curiosidad y energías para aprender cosas nuevas. 
+              <br/>
+              El desarrollo web es un área que me fascina por la capacidad de poder
               hacer realidad ideas y soluciones que pueden modificar la vida de
               muchas personas. Además de ser algo en constante crecimiento e
               innovación.
